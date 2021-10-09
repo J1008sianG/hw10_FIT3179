f77c9a1309c8eca56606ad29e537969b0584c11f
J1008sianG <90944809+J1008sianG@users.noreply.github.com>
2021-10-09 22:46:20 +0800

    Add files via upload

---
+++ plot.js
@@ -0,0 +1,4 @@
+var vg_2 = "scatterPlot.json";
+vegaEmbed("#scatterPlot", vg_2).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
